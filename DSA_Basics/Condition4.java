47e7c8b5c2ed698fb0e08b7bbc682a76284fac47
Siddhanttade <siddhanttade19@gmail.com>
2025-01-18 04:42:47 +0000

    Renaming

---
+++ DSA_Basics/Condition4.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Condition4{
+    public static void main(String[] args) {
+        //if-else
+
+
+        // int salary=25400;
+        // if(salary>1000){
+        //     salary=salary+2000;
+        // }else{
+        //     salary=salary+1000;
+        // }
+        // System.out.println(salary);
+
+
+        //for loop
+
+
+        // for(int i=0;i<=5;i++){
+        //     System.out.println(i);
+        // }
+        // Scanner in=new Scanner(System.in);
+        // int num=in.nextInt();
+        // for (int i = 1; i <num ; i++) {
+        //     System.out.println(i);
+        // }
+
+
+        //while loop
+
+
+        int a=1;
+        while (a<=5) {
+            System.out.println(a);
+            a++;
+        }
+    }
+}
