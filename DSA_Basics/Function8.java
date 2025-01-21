d11d57ba45d86aba24258d0ceadfda6b2178d35b
Siddhanttade <siddhanttade19@gmail.com>
2025-01-21 18:07:54 +0000

    basics of functions

---
+++ DSA_Basics/Function8.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class Function8 {
+    public static void main(String[] args) {
+    //     int ans=sum2();
+    //     System.out.println(ans);
+    //     sum();
+    //     String message=greet();
+    //     System.out.println(message);
+    //     int res=sum3(1, 2);
+    //     System.out.println(res);
+    // }    
+
+    // public static int sum3(int a,int b){
+    //     int sum=a+b;
+    //     return sum;
+    // }
+    // // for getting certain return value
+    // public static int sum2(){
+        // Scanner in=new Scanner(System.in);
+        // int num1,num2,sum;
+
+        // System.out.println("enter num1");
+        // num1=in.nextInt();
+        // System.out.println("2nd no");
+        // num2=in.nextInt();
+        // System.out.println();
+        // sum=num1+num2;
+        // return sum;
+
+        // Scanner in=new Scanner(System.in);
+        // System.out.println("enter your name");
+        // String name=in.next();
+        // String personalised=myGreet(name);
+        // System.out.println(personalised);
+        String chacha="Iron man";
+        greet(chacha);
+
+    }
+
+
+    // public static void sum(){
+    //     Scanner in=new Scanner(System.in);
+    //     int num1,num2,sum;
+
+    //     System.out.println("enter num1");
+    //     num1=in.nextInt();
+    //     System.out.println("2nd no");
+    //     num2=in.nextInt();
+    //     System.out.println();
+    //     sum=num1+num2;
+    //     System.out.println("sum is" +sum);
+    // }
+
+    //for string
+    // public static String greet(){
+    //     String greeting="how are you";
+    //     return greeting;
+    // }
+
+    // public static String myGreet(String name){
+    //     String message="hello"+name;
+    //     return message;
+    // }
+    public static void greet(String naam){
+        System.out.println(naam);
+    }
+}
+ 
