27d0a757f043b70eaf4413c9433b03920ea514e4
Siddhanttade <siddhanttade19@gmail.com>
2025-04-14 19:16:42 +0000

    Doubly Linked List and its manipulation

--- Linked_List/Main.java
+++ Linked_List/Main.java
@@ -1,19 +1,30 @@
 public class Main{
     public static void main(String[] args){
-        LL1 List=new LL1();
+        // LL1 List=new LL1();
+        // List.insertFirst(3);
+        // List.insertFirst(3);
+        // List.insertFirst(3);
+        // List.insertFirst(3);
+        // List.insertFirst(3);
+        // List.insertlast(2);
+        // List.insert(100, 3);
+        // System.out.println(List.deleteFirst());
+        // List.display();
+        // System.out.println(List.deleteLast());
+        // List.display();
+        // System.out.println(List.delete(3));
+        // List.display();
+
+        LL2 List=new LL2();
         List.insertFirst(3);
-        List.insertFirst(3);
-        List.insertFirst(3);
-        List.insertFirst(3);
-        List.insertFirst(3);
-        List.insertlast(2);
-        List.insert(100, 3);
-        System.out.println(List.deleteFirst());
+        List.insertFirst(6);
+        List.insertFirst(4);
+        List.insertFirst(5);
+        List.insertFirst(1);
+        List.insertLast(67);
+        List.insert(5, 0);
         List.display();
-        System.out.println(List.deleteLast());
-        List.display();
-        System.out.println(List.delete(3));
-        List.display();
+        List.displayRev();
 
     }
 }
