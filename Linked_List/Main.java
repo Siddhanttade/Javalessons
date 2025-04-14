67951b31aa9f239f887f37936174dcb24c59750c
Siddhanttade <siddhanttade19@gmail.com>
2025-04-14 19:43:33 +0000

    Circular Linked List manipulation

--- Linked_List/Main.java
+++ Linked_List/Main.java
@@ -15,16 +15,26 @@ public class Main{
         // System.out.println(List.delete(3));
         // List.display();
 
-        LL2 List=new LL2();
-        List.insertFirst(3);
-        List.insertFirst(6);
-        List.insertFirst(4);
-        List.insertFirst(5);
-        List.insertFirst(1);
-        List.insertLast(67);
-        List.insert(5, 0);
+        // LL2 List=new LL2();
+        // List.insertFirst(3);
+        // List.insertFirst(6);
+        // List.insertFirst(4);
+        // List.insertFirst(5);
+        // List.insertFirst(1);
+        // List.insertLast(67);
+        // List.insert(5, 0);
+        // List.display();
+        // List.displayRev();
+
+        LL3 List=new LL3();
+        List.insert(1);
+        List.insert(6);
+        List.insert(14);
+        List.insert(67);
+        List.insert(15);
+        List.delete(14);
+
         List.display();
-        List.displayRev();
 
     }
 }
